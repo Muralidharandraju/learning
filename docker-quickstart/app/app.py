015d56ec91c5c757cff9a79a5860bd4df9b50ad6
Muralidhar <andrajumuralidhar@gmail.com>
2024-12-07 11:28:25 +0530

    changes in app

--- docker-quickstart/app/app.py
+++ docker-quickstart/app/app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+import uvicorn
 app = FastAPI()
 
 
@@ -7,3 +7,5 @@ app = FastAPI()
 def hello_world():
     return {"message": "OK"}
 
+if __name__ == '__main__':
+    uvicorn.run("app:app",host='0.0.0.0',port=80)
