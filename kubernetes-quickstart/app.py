721ac8403b2d4b2ea6ecff61fc64afe0e885d7fb
Muralidhar <muralidharandraju@gmail.com>
2025-03-28 15:41:00 +0530

    additions in kubernetes learning

---
+++ kubernetes-quickstart/app.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+import uvicorn
+app = FastAPI()
+
+
+@app.get("/")
+def hello_world():
+    return {"message": "You are now working on the Kubernetes"}
+
+if __name__ == '__main__':
+    uvicorn.run("app:app",host='0.0.0.0',port=80)
