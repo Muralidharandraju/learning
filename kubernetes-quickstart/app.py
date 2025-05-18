00c5baac684061d3cadaf9530f9bff3c301dd52b
Muralidhar <muralidharandraju@gmail.com>
2025-05-18 17:08:52 +0530

    addition of health check

--- kubernetes-quickstart/app.py
+++ kubernetes-quickstart/app.py
@@ -8,5 +8,11 @@ def hello_world():
     return {"message": "You are now working on the Kubernetes"}
 
 
+@app.get("/hello")
+def hello_world():
+    return {"message": "You are now working on the Kubernetes"}
+
+
+
 if __name__ == '__main__':
     uvicorn.run("app:app",host='0.0.0.0',port=80)
